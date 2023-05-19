a1f48aca10a5d8eb416cc84a9e74262c93ab95a1
Edoardo <edoardobalzano01@gmail.com>
2023-05-19 12:35:19 +0200

    feat: general changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
+  theme: { 
+    extend: {
+      fontFamily: {
+        didat: "didact-gothic, Times New Roman, serif",
+        work: "work-sans, Times New Roman, serif",
+      },
+    },
   },
   plugins: [],
 };
