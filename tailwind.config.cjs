a75baebd70dc0962cf4f263e173c35d6164ff0f7
Edoardo Balzano <edoardobalzano01@gmail.com>
2023-05-21 18:16:10 +0200

    feat: add new fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: { 
+  theme: {
     extend: {
       fontFamily: {
-        didat: "didact-gothic, Times New Roman, serif",
-        work: "work-sans, Times New Roman, serif",
+        didat: ["Montserrat", "Times New Roman, serif"],
+        work: ["Outfit", "Times New Roman", "serif"],
       },
     },
   },
