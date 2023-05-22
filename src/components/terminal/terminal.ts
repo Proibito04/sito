beed96a186a0a2a514dd445ffbe3fd689bf5b1d7
Edoardo Balzano <edoardobalzano01@gmail.com>
2023-05-22 23:57:15 +0200

    feat: upgrade terminal

--- src/components/terminal/terminal.ts
+++ src/components/terminal/terminal.ts
@@ -2,21 +2,25 @@ import type { Terminal, ITheme } from 'xterm';
 import 'xterm/css/xterm.css';
 
 let term: Terminal;
+const PREFIX = "\u001b[92m$>\u001b[0m";
+
 
 export const ThemeTerminal: ITheme = {
   background: '#080F09'
 }
 
 
-export function initializeTerminal(terminal: Terminal, element: HTMLElement) {
+export async function initializeTerminal(terminal: Terminal, element: HTMLElement) {
   term = terminal;
+  const fA = await import("xterm-addon-fit");
+  const fitAddon = new fA.FitAddon();
+  terminal.loadAddon(fitAddon);
   term.open(element);
+  fitAddon.fit();
   let command = '';
   let hasInput = false;
 
-  print('Welcome *to* the *terminal!*');
-
-  let placeholder = "$>\u001b[38;5;250m type \"help\" for start...";
+  let placeholder = `${PREFIX}\u001b[38;5;250m type \"help\" for start...`;
   const ANSI_COLOR_RESET = "\u001b[0m";
   const ANSI_COLOR_CYAN = "\u001b[36m";
   const promptSymbol = `${ANSI_COLOR_CYAN}>${ANSI_COLOR_RESET}`;
@@ -30,11 +34,10 @@ export function initializeTerminal(terminal: Terminal, element: HTMLElement) {
     }
 
     const printable = !key.charCodeAt(0).toString().startsWith('27');
-    console.log(key.charCodeAt(0).toString().startsWith('27'));
 
     switch (key) {
       case '\r': // Enter key
-        term.writeln(`\r\n${promptSymbol}You typed: ${command} \u001b[0m`);
+        term.write(`\r\nYou typed: ${command} \u001b[0m\r\n${PREFIX} `);
         command = '';
         break;
       case '\u007F': // Backspace key
@@ -56,8 +59,7 @@ export function initializeTerminal(terminal: Terminal, element: HTMLElement) {
 function print(text: string) {
   let result = text.replace(/\*([^\*]+)\*/g, '\x1b[1m$1\x1b[22m');
   result = result.replace(/-([^-]+)-/g, '{$1}');
-  
+
   term.writeln(result);
-  
 }
 
