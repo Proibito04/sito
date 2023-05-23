aaff8618c49c8214d0593ba1c05853a7ce116fb8
Edoardo <edoardobalzano01@gmail.com>
2023-05-24 00:23:16 +0200

    Revert "feat: improve terminal"
    This reverts commit 836eb74b40172dc2c38e4e9d0a5a1e661d7d6ff1.

--- src/components/terminal/terminal.ts
+++ src/components/terminal/terminal.ts
@@ -2,11 +2,19 @@ import type { Terminal, ITheme } from 'xterm';
 import 'xterm/css/xterm.css';
 
 let term: Terminal;
-const PREFIX = "\u001b[92m$>\u001b[0m";
+const ANSI_COLOR_RESET = "\u001b[0m";
+const ANSI_COLOR_CYAN = "\u001b[36m";
+const ANSI_COLOR_YELLOW = "\u001b[33m";
+const ANSI_COLOR_MAGENTA = "\u001b[35m";
+const ANSI_COLOR_GREEN = "\u001b[92m";
+const PREFIX = `${ANSI_COLOR_GREEN}$>${ANSI_COLOR_RESET} `;
+
+const NEW_LINE = `\r\n${PREFIX}`;
 
 
 export const ThemeTerminal: ITheme = {
-  background: '#080F09'
+  background: '#080F09',
+
 }
 
 
@@ -20,29 +28,50 @@ export async function initializeTerminal(terminal: Terminal, element: HTMLElemen
   let command = '';
   let hasInput = false;
 
-  print('Welcome *to* the *terminal!*');
+  print('Welcome *to* the *terminal!*\n');
 
-  let placeholder = `${PREFIX}\u001b[38;5;250m type \"help\" for start...`;
-  const ANSI_COLOR_RESET = "\u001b[0m";
-  const ANSI_COLOR_CYAN = "\u001b[36m";
-  const ANSI_COLOR_YELLOW = "\u001b[33m";
 
-  const promptSymbol = `${ANSI_COLOR_CYAN}>${ANSI_COLOR_RESET} `;
-  let placeholder = `${promptSymbol}\u001b[38;5;250m type \"help\" for start...`;
+
+  let placeholder = `${PREFIX}\u001b[38;5;250mtype \"help\" for start...`;
   term.write(placeholder);
-  term.onKey(({ key }) => {
-
-    if (!hasInput) {
+  term.onData((args) => {
+    const key = args;
     
-      term.write(`\r${' '.repeat(placeholder.length)} \r${promptSymbol}`);
+    if (!hasInput) {
+      term.write(`\r${' '.repeat(placeholder.length)} \r${PREFIX}`);
       hasInput = true;
     }
 
-    const printable = !key.charCodeAt(0).toString().startsWith('27');
+    const printable = key.charCodeAt(0) > 31 && key.charCodeAt(0) < 127;
 
-      switch (key) {
-      case '\r': // Enter key
-        term.write(`\r\nYou typed: ${command} \u001b[0m\r\n${PREFIX} `);
+    switch (key) {
+      case '\r':
+      case '\u0013': // Enter key
+        const commandPrompt: string = /^\w+/.exec(command)?.[0] ?? '';
+        switch (commandPrompt) {
+          case "":
+            term.write(NEW_LINE);
+            break;
+          case 'help':
+            term.write(`\n\r${ANSI_COLOR_CYAN}help${ANSI_COLOR_RESET} - show this help
+
+            \r${PREFIX}`);
+
+            /** TODO
+            \r${ANSI_COLOR_CYAN}clear${ANSI_COLOR_RESET} - clear the terminal screen
+            \r${ANSI_COLOR_CYAN}echo${ANSI_COLOR_RESET} - echo the input
+            \r${ANSI_COLOR_CYAN}about${ANSI_COLOR_RESET} - about me
+            \r${ANSI_COLOR_CYAN}contact${ANSI_COLOR_RESET} - contact me
+            \r${ANSI_COLOR_CYAN}projects${ANSI_COLOR_RESET} - show my projects
+            \r${ANSI_COLOR_CYAN}skills${ANSI_COLOR_RESET} - show my skills
+            \r${ANSI_COLOR_CYAN}education${ANSI_COLOR_RESET} - show my education
+            \r${ANSI_COLOR_CYAN}experience${ANSI_COLOR_RESET} - show my experience
+            \r${ANSI_COLOR_CYAN}exit${ANSI_COLOR_RESET} - exit the terminal
+            */
+            break
+          default:
+            print(`\n\rCommand not found: ${commandPrompt}\n${PREFIX}`);
+        }
         command = '';
         break;
       case '\u007F': // Backspace key
@@ -51,7 +80,7 @@ export async function initializeTerminal(terminal: Terminal, element: HTMLElemen
           command = command.slice(0, -1);
         }
         break;
-      default:
+      default:       
         if (printable) {
           term.write(key);
           command += key;
@@ -65,6 +94,6 @@ function print(text: string) {
   let result = text.replace(/\*([^\*]+)\*/g, '\x1b[1m$1\x1b[22m');
   result = result.replace(/-([^-]+)-/g, '{$1}');
 
-  term.writeln(result);
+  term.write(`${result}`);
 }
 
