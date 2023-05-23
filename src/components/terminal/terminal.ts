856f88f725fc33e6d25076043e2da192794239ea
Edoardo <edoardobalzano01@gmail.com>
2023-05-23 22:17:33 +0200

    feat: add feature to terminal

--- src/components/terminal/terminal.ts
+++ src/components/terminal/terminal.ts
@@ -16,23 +16,24 @@ export function initializeTerminal(terminal: Terminal, element: HTMLElement) {
 
   print('Welcome *to* the *terminal!*');
 
-  let placeholder = "$>\u001b[38;5;250m type \"help\" for start...";
   const ANSI_COLOR_RESET = "\u001b[0m";
   const ANSI_COLOR_CYAN = "\u001b[36m";
-  const promptSymbol = `${ANSI_COLOR_CYAN}>${ANSI_COLOR_RESET}`;
+  const ANSI_COLOR_YELLOW = "\u001b[33m";
+
+  const promptSymbol = `${ANSI_COLOR_CYAN}>${ANSI_COLOR_RESET} `;
+  let placeholder = `${promptSymbol}\u001b[38;5;250m type \"help\" for start...`;
   term.write(placeholder);
   term.onKey(({ key }) => {
 
     if (!hasInput) {
-      // rimuovi il "placeholder"
-      term.write(`\r${' '.repeat(placeholder.length)} \r`);
+    
+      term.write(`\r${' '.repeat(placeholder.length)} \r${promptSymbol}`);
       hasInput = true;
     }
 
     const printable = !key.charCodeAt(0).toString().startsWith('27');
-    console.log(key.charCodeAt(0).toString().startsWith('27'));
 
-    switch (key) {
+      switch (key) {
       case '\r': // Enter key
         term.writeln(`\r\n${promptSymbol}You typed: ${command} \u001b[0m`);
         command = '';
@@ -56,8 +57,8 @@ export function initializeTerminal(terminal: Terminal, element: HTMLElement) {
 function print(text: string) {
   let result = text.replace(/\*([^\*]+)\*/g, '\x1b[1m$1\x1b[22m');
   result = result.replace(/-([^-]+)-/g, '{$1}');
-  
+
   term.writeln(result);
-  
+
 }
 
