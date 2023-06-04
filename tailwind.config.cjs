3466f65b13515aeb72e88eaa26b5602bb57b49b6
Edoardo <edoardobalzano01@gmail.com>
2023-06-04 18:07:50 +0200

    feat: add some new feature and start the Blog

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,30 @@
+const marginHR = "2rem";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            hr: {
+              marginTop: marginHR,
+              marginBottom: marginHR,
+            },
+          },
+        },
+      },
       colors: {
         BGprimary: "#080F09",
-        primary: "#1FE041",
+        primary: "#4AF72F",
         primaryDark: "#bcdd9f",
       },
       fontFamily: {
         didat: ["Montserrat", "Times New Roman, serif"],
         work: ["Poppins", "Times New Roman", "serif"],
       },
-      
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
